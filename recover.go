1c072b27897b73523dbe03ed13a863e728507bae
thanh phu <htp1912@gmail.com>
2020-10-22 16:23:36 +0700

    add recovering middleware

---
+++ recover.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+// refer to https://medium.com/@masnun/panic-recovery-middleware-for-go-http-handlers-51147c941f9 and  http://www.golangtraining.in/lessons/middleware/recovering-from-panic.html
+func PanicHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		panic("forcing a panic")
+	})
+}
+
+func Recover(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if er := recover(); er != nil {
+				jsonBody, _ := json.Marshal(map[string]string{
+					"error": "Internal Server Error",
+				})
+				f1 := logrus.Fields{}
+				f2 := AppendFields(r.Context(), f1)
+				s := GetError(er)
+				logrus.WithFields(f2).Error(s)
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write(jsonBody)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func GetError(er interface{}) string {
+	switch x := er.(type) {
+	case string:
+		return er.(string)
+	case error:
+		err := x
+		return err.Error()
+	default:
+		return "unknown panic"
+	}
+}
